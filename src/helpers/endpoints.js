620bf0461e3646991798a85e11ccaf8362250f55
alexdewaal66 <alexdewaal@gmail.com>
2022-05-27 08:39:24 +0200

    change back end url

--- src/helpers/endpoints.js
+++ src/helpers/endpoints.js
@@ -1,6 +1,6 @@
 export const endpoints = {
-    baseURL: 'http://localhost:8080',
-    // baseURL: 'https://vessels-be-assignment.herokuapp.com',
+    // baseURL: 'http://localhost:8080',
+    baseURL: 'https://vessels-be-assignment.herokuapp.com',
     signIn: '/authenticate',
     users: '/users/',
     profile: '/profile',
